5eaef65d0c7394620d594f2101fa50e54729b49d
Michael Hwang <michaelinjeyhwang@gmail.com>
2020-12-01 23:18:18 -0500

    Added TS and axios as starter boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./styles.css";
+
+/*
+Available Packages:
+  axios
+
+Feel free to add more via npm / yarn
+
+Note that this is TypeScript
+*/
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Future OpenMealer</h1>
+      <h2>Start editing to see React do its magic!</h2>
+    </div>
+  );
+}
